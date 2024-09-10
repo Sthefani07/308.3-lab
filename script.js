402a1941f6e42076637d3b230cbf0911c144d51a
Sthefani Denhof <sdenhof7@gmail.com>
2024-09-10 13:40:07 -0700

    A loop that ckecks if a number is divisible by 3 & 5.

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Loop through all numbers from 1 to 100.
+//______________________________________ Loop through all numbers from 1 to 100.
 
 // let num = 100;
 // while (num > 0) {
@@ -6,16 +6,16 @@
 //   num--;
 // }
 
-// If a number is divisible by 3, log “Fizz.”
-for (let i = 0; i <= 20; i++) {
-  if (i % 3 === 0) {
-    console.log(i + "Buzz");
-  } else {
-    console.log(i);
-  }
-}
+// ____________________________________if a number is divisible by 3, log “Fizz.”
+// for (let i = 0; i <= 20; i++) {
+//   if (i % 3 === 0) {
+//     console.log(i + "Buzz");
+//   } else {
+//     console.log(i);
+//   }
+// }
 
-// If a number is divisible by 5, log “Buzz.”
+// ____________________________________If a number is divisible by 5, log “Buzz.”
 
 // for (let i = 0; i <= 20; i++){
 //     if (i % 5 === 0){
@@ -25,6 +25,20 @@ for (let i = 0; i <= 20; i++) {
 //     }
 // }
 
-// If a number is divisible by both 3 and 5, log “Fizz Buzz.”
+// ___________________________________If a number is divisible by both 3 and 5, log “Fizz Buzz.”
+for (let i = 0; i <= 20; i++){
+    if (i % 3 === 0 && i % 5 === 0){
+        console.log(i + "Fizz Buzz");
+    }else if (i % 3 === 0){
+        console.log(i + "Fizz Fizz");
+    }else if (i % 5 === 0){
+        console.log(i + "Fizz Buzz");
+    }else{
+        console.log(i);
+    }
+}
 
-// If a number is not divisible by either 3 or 5, log the number.
+
+
+
+// ___________________________________If a number is not divisible by either 3 or 5, log the number.
