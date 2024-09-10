837d0fe071da04137dad5dc73b9a2671c4ff5e2b
Sthefani Denhof <sdenhof7@gmail.com>
2024-09-10 16:49:39 -0700

    A loop of numbers that are NOT divisible by 3 & 5.

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//______________________________________ Loop through all numbers from 1 to 100.
+//________PART 1______________________________ Loop through all numbers from 1 to 100.
 
 // let num = 100;
 // while (num > 0) {
@@ -26,19 +26,27 @@
 // }
 
 // ___________________________________If a number is divisible by both 3 and 5, log “Fizz Buzz.”
-for (let i = 0; i <= 20; i++){
-    if (i % 3 === 0 && i % 5 === 0){
-        console.log(i + "Fizz Buzz");
-    }else if (i % 3 === 0){
-        console.log(i + "Fizz Fizz");
-    }else if (i % 5 === 0){
-        console.log(i + "Fizz Buzz");
-    }else{
-        console.log(i);
-    }
-}
-
-
-
+// for (let i = 0; i <= 20; i++){
+//     if (i % 3 === 0 && i % 5 === 0){
+//         console.log(i + "Fizz Buzz");
+//     }else if (i % 3 === 0){
+//         console.log(i + "Fizz Fizz");
+//     }else if (i % 5 === 0){
+//         console.log(i + "Fizz Buzz");
+//     }else{
+//         console.log(i);
+//     }
+// }
 
 // ___________________________________If a number is not divisible by either 3 or 5, log the number.
+
+
+// for (let i = 0; i <= 20; i++){
+//     if (i % 3 !== 0 && i % 5 !== 0){
+//         console.log(i);
+//     }
+// }
+
+
+// __________________________PART 2__________
+
