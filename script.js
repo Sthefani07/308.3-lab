0c28ef1cc6f905e55d1d5402a34838702097e54d
Sthefani Denhof <sdenhof7@gmail.com>
2024-09-10 13:02:06 -0700

    loop that checks if a number is divisible by 5.

--- script.js
+++ script.js
@@ -1,14 +1,22 @@
 // Loop through all numbers from 1 to 100.
 
-let num = 100;
-while (num > 0) {
-  console.log(num);
-  num--;
-}
+// let num = 100;
+// while (num > 0) {
+//   console.log(num);
+//   num--;
+// }
 
 // If a number is divisible by 3, log “Fizz.”
 
+
 // If a number is divisible by 5, log “Buzz.”
+for (let i = 0; i <= 20; i++){
+    if (i % 5 === 0){
+        console.log(i + "Buzz");
+    }else {
+        console.log(i)
+    }
+}
 
 // If a number is divisible by both 3 and 5, log “Fizz Buzz.”
 
